aa66b8080fbdcc690ba3a6f1eb611bca07e929a6
Ketsia Iragena <71078482+Ketsia-a@users.noreply.github.com>
2020-12-16 16:10:22 +0200

    Update manage.py

--- manage.py
+++ manage.py
@@ -18,7 +18,7 @@ manager.add_command('db',MigrateCommand)
 @manager.command
 def test():
     """Run the unit tests."""
-    import unittestflask_bootstrap
+    import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
 
